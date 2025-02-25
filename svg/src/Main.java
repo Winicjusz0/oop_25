d41215f1b7586274e1e614bc5f9a2c2ced5c920a
abc <a@b.com>
2025-02-25 17:16:08 +0100

    update

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -6,5 +6,25 @@ public class Main {
         p1.y = 4.0;
         System.out.println("P1: (" + p1.x + "," + p1.y + ")");
         System.out.println(p1.toSvg());
+
+        Segment segment = new Segment();
+        segment.a = new Point();
+        segment.a.x = 0.0;
+        segment.a.y = 0.0;
+        segment.b = new Point();
+        segment.b.x = 4.0;
+        segment.b.y = 3.0;
+
+        Segment[] arr = new Segment[2];
+        arr[0] = segment;
+    }
+
+    public static Segment maxSegmenth(Segment[] arr){
+        if(arr.length == 0)
+            return null;
+
+        Segment max = arr[0];
+        for(int i=1)
+
     }
 }
